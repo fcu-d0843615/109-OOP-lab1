cbbba6b926dc9e56a85e5d49d3bbb869378f1bb1
user <user@EC02>
2020-09-29 09:38:25 +0800

    123

---
+++ src/lab1hw/lab1hw.java
@@ -0,0 +1,9 @@
+package lab1hw;
+public class lab1hw {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm TinRon.");
+		System.out.println("My NID is D0843615.");
+		System.out.println("This is Lab1 HW.");
+	}
+}
